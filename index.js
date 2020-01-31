5189544fdec638aab8872b262e3b61c8b102dcb1
Rahul Singh <32161440+rahulsingh9878@users.noreply.github.com>
2020-01-31 11:59:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
