730bd2a0f866075fea381b3832a9f0490b244be2
Rahul Singh <32161440+rahulsingh9878@users.noreply.github.com>
2020-04-01 19:55:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 3000;
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log(msg);
     io.emit('chat message', msg);
   });
 });
